e56a3f154696a574c9929f3f4447b1090b7e60c0
NessTorres <arenee27@ksu.edu>
2016-08-23 12:56:23 -0500

    Add files via upload

---
+++ CIS 209.java
@@ -0,0 +1,37 @@
+Mitchell's CS Class
+
+****************************************************
+
+Jorge Valenzuela
+Office: DUE 2172
+Hours: MTW 1-2 U 11-12
+
+Lab Instructor: Sharmila Vegesana
+
+****************************************************
+
+No make ups, unless under extreme cases.
+
+Naming Convention:
+	EastinM_L#.c
+	
+Grades:
+	Labs: 40%
+	Midterms: 25%
+	Final: 25%
+	Quizzes(4): 10%
+	
+	85% is an A
+	
+****************************************************
+
+Table of Contents:
+
+Class #/Date/Info
+
+
+
+****************************************************
+
+Class 1: Aug. 22, 2016
+
